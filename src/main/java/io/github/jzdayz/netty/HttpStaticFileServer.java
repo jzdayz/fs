9c7dd4d16ab10b94b7e92e594f5641909883f401
jzdayz <1397649957@qq.com>
2019-12-30 09:27:08 +0800

    cn

--- src/main/java/io/github/jzdayz/netty/HttpStaticFileServer.java
+++ src/main/java/io/github/jzdayz/netty/HttpStaticFileServer.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package io.github.jzdayz.web;
+package io.github.jzdayz.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
--- src/main/java/io/github/jzdayz/netty/HttpStaticFileServerHandler.java
+++ src/main/java/io/github/jzdayz/netty/HttpStaticFileServerHandler.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package io.github.jzdayz.web;
+package io.github.jzdayz.netty;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
